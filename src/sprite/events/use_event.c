9ba37ee774c75ecbe88b41cb75ffdb15e482c4c1
fry <jules.danet@epitech.eu>
2022-12-12 22:53:40 +0100

    [Update] tlist function [Add] Int to base

--- src/sprite/events/use_event.c
+++ src/sprite/events/use_event.c
@@ -9,10 +9,8 @@
 
 void sprite_use_event(sprite *self, window *window_datas)
 {
-    if (self->events_list->length == 0) {
-        printf("Break");
+    if (self->events_list->length == 0)
         return;
-    }
     list_foreach(self->events_list, node) {
         if (((event *)node->value)->type == window_datas->event.type)
             ((event *)node->value)->event_function(self, window_datas);
